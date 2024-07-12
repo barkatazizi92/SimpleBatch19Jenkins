8658cfdf331b8148af4c4b2a048172ece634448c
barkatazizi92 <barkatmilad@gmail.com>
2024-07-12 15:16:51 -0400

    new code added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 20; i++) {
+
+            if(i ==19) {
+                System.out.println(" But Batch " + i + " is greatest");
+            } else {
+                System.out.println("Batch "+ i + " is great ");
+            }
+            Thread.sleep(2000);
+
+        }
+
     }
 }
